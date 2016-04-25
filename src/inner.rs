22b52a1d15510eb4e93a1cb22c27f37f9414900a
Jeremy Fitzhardinge <jeremy@goop.org>
2016-04-25 01:39:37 -0700

    WIP

--- src/inner.rs
+++ src/inner.rs
@@ -1,57 +1,17 @@
-use std::marker::PhantomData;
-use crossbeam::Owned;
+use std::thread::Thread;
 use fnbox::Thunk;
 
-// Actual representation of inner - Enum tag stored in lower bits
-struct UnsafeInner<T>(usize, PhantomData<T>);
-
-impl UnsafeInner<T> {
-    const EMPTY: usize = 0;
-    const VALUE: usize = 1;
-    const WAIT: usize = 2;
-    const CALLBACK: usize = 3;
-    
-    unsafe fn empty() -> UnsafeInner<T> {
-        UnsafeInner(EMPTY, PhantomData)
-    }
-
-    unsafe fn value(v: Box<T>) -> UnsafeInner<T> {
-        let uv = v.into_raw() as usize;
-        assert!(uv & 0x03 == 0);
-        UnsafeInner(uv | VALUE, PhantomData)
-    }
-
-    unsafe fn wait(t: &Thread) -> UnsafeInner<T> {
-        let ut = t as *mut Thread as usize;
-        assert!(ut & 0x03 == 0);
-        UnsafeInner(ut | WAIT, PhantomData)
-    }
-
-    unsafe fn callback(cb: Thunk<'static, T>) -> UnsafeInner<T> {
-        let ucb = cb.into_raw() as usize;
-        assert!(ucb & 0x03 == 0);
-        UnsafeInner(ucb | CALLBACK, PhantomData)
-    }
-}
-
-impl<T> From<UnsafeInner<T>> for Inner<T> {
-    fn from(inner: UnsafeInner<T>) -> Self {
-        match inner.0 & 0x03 {
-            EMPTY => Inner::Empty,
-            VALUE => {
-                let raw = (inner.0 & !0x03) as *mut T;
-                Inner::Value(Box::from_raw(raw))
-            },
-            WAIT => {
-                
-            }
-        }
-    }
-}
-
 pub enum Inner<T> {
-    Empty,                      // No value, no waiter
-    Value(Box<T>),              // has value
-    Wait(Thread),               // Has waiter
-    Callback(Thunk<'static,T>), // Has callback
+    Empty,                       // No value, no waiter
+    Value(T),                    // Has value
+    Wait(Thread),                // Has waiter
+    Callback(Thunk<'static, T>), // Has callback
+    Dead,                        // No further use
+}
+
+impl<T> Inner<T> {
+    pub fn empty() -> Self { Inner::Empty }
+    pub fn value(v: T) -> Self { Inner::Value(v) }
+    pub fn wait(th: Thread) -> Self { Inner::Wait(th) }
+    pub fn callback(cb: Thunk<'static, T>) -> Self { Inner::Callback(cb) }
 }
